b387cc769ae8adf44aabb4112f64efeb87ea0e8f
FEstrada-0987 <florencia.estrada@hotmail.com>
2023-02-17 22:13:59 -0300

    Agregando const

--- app.js
+++ app.js
@@ -5,3 +5,86 @@ const btnBalance = document.getElementById('btn-balance');
 const btnCategorias = document.getElementById('btn-categorias');
 const btnReportes = document.getElementById('btn-reportes');
 
+
+const pagBalance = document.getElementById('pag-balance');
+const balanceGanancias = document.getElementById('balance-ganancias');
+const balanceGastos = document.getElementById('balance-gastos');
+const balanceTotal = document.getElementById('balance-total');
+
+//FILTROS
+const btnOcultarFiltro = document.getElementById('btn-ocultar-filtro');
+const filtroTipo = document.getElementById('filtro-tipo');
+const filtroCategoria = document.getElementById('filtro-categoria'); 
+const filtroOrdenar = document.getElementById('filtro-ordenar'); 
+const btnNuevaOperac = document.getElementById('btn-nueva-operac');
+//SIN OPERACIONES
+const verSinOperac = document.getElementById('ver-sin-operac'); 
+//TABLA OPERACIONES
+const tablaOperaciones = document.getElementById('tabla-operaciones'); 
+const escribirOperacion = document.getElementById('escribir-operacion');
+//NUEVA OPERACION
+const pagNuevaOperac = document.getElementById('pag-nueva-operac');
+const inputDescripcion = document.getElementById('input-descripcion');
+const inputMonto = document.getElementById('inputMonto');
+const categoriaOperacionSelect = document.getElementById('categoria-operacion-select');
+const inputfecha = document.getElementById('input-fecha'); 
+const btnCancelar = document.getElementById('btn-cancelar'); 
+const btnAgregar = document.getElementById('btn-agregar');
+
+//----------------EDITAR OPERACIONES
+
+const paginaEditarOperac = document.getElementById('pagina-editar-operac'); 
+const inputEditarDescripcion = document.getElementById('input-editar-descripcion');
+const inputEditarMonto = document.getElementById('input-editar-monto'); 
+const inputEditarTipo = document.getElementById('input-editar-tipo');
+const categoriaOperacEditarSelect = document.getElementById('categoria-operac-editar-select');
+const inputEditarFecha = document.getElementById('input-editar-fecha');
+const btnCancelarEditar = document.getElementById('btn-cancelar-editar');
+const btnEditarEditar = document.getElementById('btn-editar-editar');
+
+//-----------------------------CATEGORIAS----------------------------------
+
+
+const paginacateg = document.getElementById('pagina-categ'); 
+const agregarCategoriaBoton = document.getElementById('agregar-categoria-boton');
+const listaCategorias = document.getElementById('lista-categorias');
+
+
+//-------------------Reportes
+
+const paginaReportes = document.getElementById('pagina-reportes'); 
+
+//-----------------Seccion sin los reportes
+
+const sinReportes = document.getElementById('sin-reportes');
+
+//----------------Seccion con reportes
+
+const conReportes = document.getElementById('con-reportes'); 
+const reporteResumen = document.getElementById('reporte-resumen');
+
+const reporteTotalCategorias = document.getElementById('reporte-total-categorias'); 
+const reporteTotalMes = document.getElementById('reporte-total-mes');
+
+const editarCategoria = document.getElementById('editar-categoria');
+const inputEditarCategoria = document.getElementById('input-editar-categoria');
+const btnCancelEditCategory = document.getElementById('btn-cancel-edit-category');
+const btnEditEditCategory = document.getElementById('btn-edit-edit-category');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
