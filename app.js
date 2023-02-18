5349461253f3876b77c3a7f090359552b74978ba
FEstrada-0987 <florencia.estrada@hotmail.com>
2023-02-17 21:26:20 -0300

    Agregando estructura HTML completa

---
+++ app.js
@@ -0,0 +1,7 @@
+//------------------BOTONES---------------//
+const burgerMenu = document.getElementById('navbar-burger');
+const navbarMenu = document.getElementById('navbar-menu');
+const btnBalance = document.getElementById('btn-balance');
+const btnCategorias = document.getElementById('btn-categorias');
+const btnReportes = document.getElementById('btn-reportes');
+
